a0940bc6b8d4836ed1453f1c934f01a48271e057
huanminwu <55599948+huanminwu@users.noreply.github.com>
2019-12-31 16:07:56 -0800

    Rename LeetCodeSort.h to LeetCode/LeetCodeSort.h

--- LeetCode/LeetCodeSort.h
+++ LeetCode/LeetCodeSort.h
@@ -407,7 +407,7 @@ public:
     /// Given an integer array nums, return the number of range sums that 
     /// lie in [lower, upper] inclusive.
     /// Range sum S(i, j) is defined as the sum of the elements in nums 
-    /// between indices i and j (i ¡Ü j), inclusive.
+    /// between indices i and j (i Â¡Ãœ j), inclusive.
     /// 
     /// Note:
     /// A naive algorithm of O(n^2) is trivial. You MUST do better than that. 
