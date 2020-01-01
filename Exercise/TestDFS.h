3962dc9319a41173c9dbc801761e68c68dafb770
huanminwu <huanminwu@hotmail.com>
2019-12-31 17:42:34 -0800

    Update TestDFS.h

--- Exercise/TestDFS.h
+++ Exercise/TestDFS.h
@@ -2,6 +2,7 @@
 #ifndef TestDFS_H
 #define TestDFS_H
 #include "..\LeetCode\LeetCodeDFS.h"
+void TestLeetCode1066(void);
 void TestLeetCode1219(void);
 void TestLeetCode1240(void);
 void TestLeetCode1286(void);
