29aa3c547fa039ccf0aae5100d18be90d5ab67cf
Huanmin Wu <huanminw@microsoft.com>
2024-06-01 13:48:21 -0700

    check in weekly contest

--- LeetCode/LeetCodeMath.cpp
+++ LeetCode/LeetCodeMath.cpp
@@ -22488,5 +22488,65 @@ int LeetCodeMath::numberOfPairsI(vector<int>& nums1, vector<int>& nums2, int k)
     return result;
 }
 
+/// <summary>
+/// LeetCode 3164. Find the Number of Good Pairs II
+/// 
+/// Medium
+///
+/// You are given 2 integer arrays nums1 and nums2 of lengths n and m 
+/// respectively. You are also given a positive integer k.
+///
+/// A pair (i, j) is called good if nums1[i] is divisible by 
+/// nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1).
+///
+/// Return the total number of good pairs.
+/// 
+/// Example 1:
+/// Input: nums1 = [1,3,4], nums2 = [1,3,4], k = 1
+///
+/// Output: 5
+/// Explanation:
+/// The 5 good pairs are (0, 0), (1, 0), (1, 1), (2, 0), and (2, 2).
+///
+/// Example 2:
+/// Input: nums1 = [1,2,4,12], nums2 = [2,4], k = 3
+/// Output: 2
+/// Explanation:
+/// The 2 good pairs are (3, 0) and (3, 1).
+/// 
+/// Constraints:
+/// 1. 1 <= n, m <= 10^5
+/// 2. 1 <= nums1[i], nums2[j] <= 10^6
+/// 3. 1 <= k <= 10^3
+/// </summary>
+long long LeetCodeMath::numberOfPairsII(vector<int>& nums1, vector<int>& nums2, int k)
+{
+    unordered_map<int, int> num_count;
+    for (size_t i = 0; i < nums2.size(); i++)
+    {
+        num_count[nums2[i]]++;
+    }
+    long long result = 0;
+    for (size_t i = 0; i < nums1.size(); i++)
+    {
+        if (nums1[i] % k != 0) continue;
+        int n = nums1[i] / k;
+        for (int j = 1; j <= (int)sqrt(n); j++)
+        {
+            if (n % j != 0) continue;
+            if (num_count.count(j) > 0)
+            {
+                result += (long long)num_count[j];
+            }
+            if (n / j == j) continue;
+            if (num_count.count(n / j) > 0)
+            {
+                result += (long long)num_count[n / j];
+            }
+        }
+    }
+    return result;
+}
+
 #pragma endregion
 
